48e6471138c9c339f5f33098eb9c41d79abec6f7
Jordan Réjaud <jrejaud@gmail.com>
2016-11-07 13:21:36 -0800

    Update cheat-sheet.ino

--- cheat-sheet.ino
+++ cheat-sheet.ino
@@ -5,7 +5,7 @@
 #define CLOCK_PIN 13 //Serial Clock output (don't change this)
 CRGB leds[NUMBER_OF_LEDS]; //LED Array (don't change this)
 
-//Buttons (Don't change any of this)
+//Button Pins (Don't change any of this)
 #define BUTTON_ONE_PIN 6 
 #define BUTTON_TWO_PIN 5 
 #define BUTTON_THREE_PIN A2 
@@ -13,3 +13,6 @@ CRGB leds[NUMBER_OF_LEDS]; //LED Array (don't change this)
 #define BUTTON_TRIANGLE_PIN 3
 #define BUTTON_CIRCLE_PIN 4
 #define BUTTON_SQUARE_PIN 8 
+
+//Buzzer Pin (Don't change any of this)
+#define BUZZER_PIN 7 //Untested
